38ef41a00e56fa129b3405980895e3df4d0ba2dd
vikasGitAcc <vikassindhad12345@gmail.com>
2026-09-17 14:12:07 +0530

    graph setup completed

--- backend/agent.js
+++ backend/agent.js
@@ -1,16 +1,62 @@
-import { StateGraph } from "@langchain/langgraph";
+import { END, MessagesAnnotation, StateGraph } from "@langchain/langgraph";
 import { model } from "./src/model/llm.model.js";
 import { getEvent, createEvent } from "./src/prepare/calendar.prepare.js";
+import { ToolNode } from "@langchain/langgraph/prebuilt";
+import { HumanMessage } from "@langchain/core/messages";
 
 const tools = [getEvent, createEvent]
 
 const LLMWithTools = model.bindTools(tools);
 
-(async function main(){
-    const res = await LLMWithTools.invoke("create a meeting in google calendar with sujoy on 15th sep 2026, meeting goint to held in google meet at 3:00 pm")
+const graph = new StateGraph(MessagesAnnotation)
 
-    console.log(res)
+/**
+ * model node
+ */
+
+const assistant = async(state) => {
+   const response = await LLMWithTools.invoke(state.messages);
+   if(!response){
+    throw new Error("LLM failed to generate the output")
+   }
+   return {messages:[response]}   
+}
+
+/**
+ * Tool Node
+ */
+
+const toolNode = new ToolNode(tools);
+
+/**
+ * conditional edge decider
+ */
+
+async function shouldContinue(state) {
+    const lastMessage = state.messages[state.messages.length-1];
+
+    if(lastMessage.tool_calls?.length>0) return "tools"
+    
+    return "__end__"
+}
+
+/**
+ * build the graph
+ */
+
+graph
+.addNode("assistant", assistant)
+.addNode("tools", toolNode)
+.addEdge("__start__", "assistant")
+.addEdge("tools", "assistant")
+.addConditionalEdges("assistant", shouldContinue, {"__end__": END, "tools": "tools"})
+
+const app = graph.compile();
+
+(async function main(){
+    const res = await app.invoke({messages: new HumanMessage("create a meeting in google calendar with sujoy on 15th sep 2026, meeting goint to held in google meet at 3:00 pm")})
+
+    console.log((res.messages[res.messages.length-1]).content);
 })()
 
-// const graph = new StateGraph()
 
